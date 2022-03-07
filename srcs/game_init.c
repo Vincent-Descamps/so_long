d66397bd5c228217432ed91e852735e566e49c40
Vincent Descamps <vincedescamps@gmail.com>
2022-03-07 09:24:36 +0100

    gnl

--- srcs/game_init.c
+++ srcs/game_init.c
@@ -6,7 +6,7 @@
 /*   By: vdescamp <vdescamp@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 16:22:35 by vdescamp          #+#    #+#             */
-/*   Updated: 2022/03/04 15:28:08 by vdescamp         ###   ########.fr       */
+/*   Updated: 2022/03/07 09:24:36 by vdescamp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,4 @@ void	game_init(t_game *game)
 	game->map.width = 0;
 	game->map.height = 0;
 }
+f
